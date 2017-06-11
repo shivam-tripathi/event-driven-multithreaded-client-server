b19a05ec31c924fd3721f860ead91f096d9e7845
Shivam Tripathi <shivam.flash@gmail.com>
2017-06-11 19:45:58 +0530

    Remove process metadata, add connection info, add sigchld handler and process map

--- src/router/utils.h
+++ src/router/utils.h
@@ -14,38 +14,39 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <set>
 #include <vector>
 #include <utility>
 
 // using namespace std;
 
-// Enum stores various possible actions 
+// Enum stores various possible actions
 // RTC : Request to connect
-// FTR : File transfer request 
-// CJA : Chat join action 
-// CPA : Chat post action 
+// FTR : File transfer request
+// CJA : Chat join action
+// CPA : Chat post action
 
 enum Action {
 	RTC,
 	FTR,
-	CJA, 
+	CJA,
 	CPA
 };
 
-// Pack type type 
-// COM : Communication generic packet, small in size - only for general purpose message passing 
+// Pack type type
+// COM : Communication generic packet, small in size - only for general purpose message passing
 // DATA : Data packet - used in file transfer, of nearly 10 kB size
 enum PackType {
-	COM, 
+	COM,
 	DATA
 };
 
 // Header part contains header information - action and number of distinct messages
 // This is present in all packets
 // Packet struct encapsulates the header and data into a single packet
-// If just a generic packet, the data will point to NULL 
-// src_id is the connection id for the packet source 
-// dest_id is the connection id for the packet destination 
+// If just a generic packet, the data will point to NULL
+// src_id is the connection id for the packet source
+// dest_id is the connection id for the packet destination
 // By default src_id and dest_id for the server is 0
 
 struct Packet {
@@ -57,7 +58,7 @@ struct Packet {
 };
 
 // Class to create and deliver a complete packet
-class Pack { 
+class Pack {
 	Packet packet;
 	void pack_header();
 	std::vector<std::string> msg;
@@ -71,15 +72,19 @@ public:
 	Packet get_packet();
 };
 
-struct process_metadata {
-	int sockfd;
-	char sender_id[20];
-	char receiver_id[20];
-	sockaddr_storage *sender_saddr;
-	sockaddr_storage *receiver_saddr;
+struct connection_info {
+	pid_t pid;
+	int sock_fd;
+	std::string str;
+	connection_info() {}
+	connection_info(pid_t pid, int fd, std::string str) : pid(pid), sock_fd(fd), str(str) {}
 };
 
 // Some other generic utility functions
+
+extern std::set<pid_t> running_processes;
+
+void sigchld_handler(int s);
 
 int hostname_to_ip(char *, char*);
 
